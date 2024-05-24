ce3e7c4866ea8c9facd0cf5df94ba266feb10207
JessieYan214 <jessieyan214@gmail.com>
2024-05-24 12:13:13 +1200

    Initializing the project

---
+++ Program.cs
@@ -0,0 +1,57 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors((options) =>
+{
+    // Adding CORS policies for different environments
+
+    // Development CORS policy allows requests from localhost ports 4200, 3000, and 8000
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+
+    // Production CORS policy allows requests only from the specified production site
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("https://myProductionSite.com")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseCors("DevCors");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
+}
+
+app.MapControllers();
+
+// app.MapGet("/weatherforecast", () =>
+// {
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
+
+app.Run();
